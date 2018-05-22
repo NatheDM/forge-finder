57ccfc9ffbe5234a28f46f0ac1e62a66d8a7625c
Jonathan Moss <nathaniel.coldfyre@gmail.com>
2018-05-22 15:15:29 -0700

    Added Bootstrap-y and style stuff

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header.js";
+import "./app.css";
+
+const App = props => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
