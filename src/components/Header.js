b6c92afbbfb96055ea0b8737332c063248a08203
Jonathan Moss <nathaniel.coldfyre@gmail.com>
2018-05-23 10:30:44 -0700

    Now with better beards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, NavItem, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import beard from "./beard.svg";
+import Beard from "./Beard.js";
 
 const Header = props => {
   return (
@@ -9,7 +9,7 @@ const Header = props => {
       <Navbar.Header>
         <Navbar.Brand>
           <a href="#" style={{ display: "flex" }}>
-            <img src={beard} height={20} alt="brand-logo" />
+            <Beard />
             ForgeFindr
           </a>
         </Navbar.Brand>
@@ -18,13 +18,13 @@ const Header = props => {
         <LinkContainer exact to="/">
           <NavItem eventKey={1}>Home</NavItem>
         </LinkContainer>
-        <LinkContainer exact to="/profile">
+        <LinkContainer to="/profile">
           <NavItem eventKey={2}>Profile</NavItem>
         </LinkContainer>
-        <LinkContainer exact to="/login">
+        <LinkContainer to="/login">
           <NavItem eventKey={3}>Login</NavItem>
         </LinkContainer>
-        <LinkContainer exact to="/logout">
+        <LinkContainer to="/logout">
           <NavItem eventKey={4}>Logout</NavItem>
         </LinkContainer>
       </Nav>
